23bc4ee3b8e25f2fda1d36cf24f5557920f33ffd
Charlie.Bonel <charliebonel96@gmail.com>
2026-01-06 20:31:03 +0000

    adding movement to character

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ def main():
         screen.fill("black")
         player.draw(screen)
         player.update(dt)
+        #player.move(dt)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 import pygame
+import math
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -21,6 +22,20 @@ class Player(CircleShape):
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
+        return self.rotation
+
+    def move(self, dt):
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector
+        # print(self.rotate(dt))
+        # x2 = math.cos(rotation) * vector_x - math.sin(rotation) * vector_y
+        # y2 = math.sin(rotation) * vector_y - math.cos(rotation) * vector_y
+         #vector_x = x2 * PLAYER_SPEED * dt
+        # vector_y = y2 * PLAYER_SPEED * dt
+        # vector = [x2, y2]
+        # print(f"The vector is: {vector}")
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
@@ -29,3 +44,7 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
