0326737d132488fce1726489ff6b5481e67f9a26
Charlie.Bonel <charliebonel96@gmail.com>
2026-01-06 17:43:18 +0000

    adding player character sprite

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
-from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH, PLAYER_RADIUS
 from logger import log_state
+from player import Player
 
 
 def main():
@@ -10,13 +11,23 @@ def main():
     # initialising game loop
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    # add clock for delta time
+    clock = pygame.time.Clock()
+    dt = 0
+    # instantiating player
+    screen_x = SCREEN_WIDTH / 2
+    screen_y = SCREEN_HEIGHT / 2
+    player = Player(screen_x, screen_y, PLAYER_RADIUS)
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
